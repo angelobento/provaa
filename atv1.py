fee9ca6b646bf745ce93c868d7c1c21d8d6f933f
angelobento <49735006+angelobento@users.noreply.github.com>
2019-09-13 20:34:47 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,28 @@
+#atividade 1
+palavra = str(input("digite uma frase "))
+print("a nova frase ficou: {}".format('p'.split()[0,]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ atv2.py
@@ -0,0 +1,21 @@
+#atividade 2: data
+# data = input("dgite uma data por extenso: ")
+# dia = int[0, 33]
+# mes = str("janeiro", "fevereiro", "março,", "abril", "maio", 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro')
+# ano = int
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
